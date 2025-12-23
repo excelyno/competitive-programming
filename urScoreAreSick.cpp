48d84c326970c4d81cb1cd8ef6d6bcf717981008
excelyno <excellentqweee@gmail.com>
2025-12-23 19:20:22 +0700

    hope its not my gpa

---
+++ urScoreAreSick.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+int main(){
+    cin >> n;
+    if (n >=75){
+        cout << "lulus";
+    } else {
+        cout << "tidak lulus";
+    }
+}
